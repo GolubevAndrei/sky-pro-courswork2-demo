5825a0ba5233fb3c5528aff272836c12cfa10dff
GolubevAndrey <golubev_andrei87@mail.ru>
2022-11-21 16:51:05 +0300

    Initial commit

---
+++ src/main/java/sky/pro/skyprocourswork2demo/SkyProCourswork2DemoApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.skyprocourswork2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProCourswork2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyProCourswork2DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/skyprocourswork2demo/SkyProCourswork2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.skyprocourswork2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProCourswork2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
