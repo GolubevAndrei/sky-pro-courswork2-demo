06b5f1a0ceede33a9024dd662d20c0a240206f76
GolubevAndrey <golubev_andrei87@mail.ru>
2022-11-21 16:54:36 +0300

    Question

---
+++ src/main/java/sky/pro/skyprocourswork2demo/Question/Question.java
@@ -0,0 +1,19 @@
+package sky.pro.skyprocourswork2demo.Question;
+
+public class Question {
+    String question;
+    String answer;
+
+    public Question(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
